fa30e1344816add922e81b56501822022eeecdea
hulu1109 <huhuimin1109@gmail.com>
2016-05-12 15:10:13 +0800

    new project realized collect data

---
+++ app/src/main/java/huami/com/recognitiondemo/AccelerometerService.java
@@ -0,0 +1,146 @@
+package huami.com.recognitiondemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+
+import huami.com.recognitiondemo.utils.SensorBuffer;
+import huami.com.recognitiondemo.utils.SensorModel;
+
+/**
+ * Created by test on 2016/5/12.
+ */
+public class AccelerometerService extends Service{
+    public final static String ACTION_HEADER = "com.huami.recognitiondemo.";
+    private final static String TAG = "SensorACC";
+
+
+
+    protected SensorManager mSensorManager;
+    protected Sensor mSensor;
+    protected int mAccuracy;
+
+    protected boolean mIsSensorReady = false;
+    protected int mSampleCount = 0;
+    protected File mTargetFile;
+
+
+    //public static Date sStartDate = null;
+   // protected String mName = "ACC";
+    protected int mColumnNum = 3;
+    protected String mColumnHeader = "AccX\tAccY\tAccZ";
+
+
+    private FileOutputStream mWriter;
+    private boolean mPrintLock = false;
+
+
+
+    public SensorBuffer sensorBuffer;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorBuffer = new SensorBuffer();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mIsSensorReady = mSensorManager.registerListener(
+            sensorEventListener, mSensor, SensorManager.SENSOR_DELAY_FASTEST
+        );
+        startRecord();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        if(mIsSensorReady){
+            mSensorManager.unregisterListener(sensorEventListener);
+            mIsSensorReady = false;
+        }
+        stopRecord();
+        super.onDestroy();
+    }
+
+    private final SensorEventListener sensorEventListener;
+    {
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                ++mSampleCount;
+
+                float[] fixedValues = new float[mColumnNum];
+
+                fixedValues[0] = event.values[0];
+                fixedValues[1] = event.values[1];
+                fixedValues[2] = event.values[2];
+
+                SensorModel acc;
+                acc = new SensorModel(fixedValues[0], fixedValues[1], fixedValues[2]);
+                sensorBuffer.add(acc);
+
+                String line = Float.toString(fixedValues[0]);
+                for(int i = 1; i <mColumnNum; ++i)
+                    line += "\t" + fixedValues[i];
+                printLine(line + "\n");
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                mAccuracy = accuracy;
+            }
+        };
+    }
+
+    public void startRecord(){
+        mTargetFile = new File(MainActivity.sSensorBaseFolder.getPath(), MainActivity.DATE_FORMAT.format(MainActivity.sStartDate) + ".txt");
+        try{
+            mWriter = new FileOutputStream(mTargetFile);
+            mWriter.write((MainActivity.DATE_FORMAT.format(MainActivity.sStartDate) + "\n").getBytes());
+            mWriter.write(("Timestamp\t" + mColumnHeader + "\n").getBytes());
+            if(!mIsSensorReady)
+                mWriter.write(("Sensor initialization failed\n").getBytes());
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    public void stopRecord(){
+        try {
+            mWriter.close();
+        }catch (Exception e){
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    protected void printLine(String line){
+        try{
+            while(mPrintLock)
+                continue;
+            mPrintLock = true;
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            mWriter.write((df.format(System.currentTimeMillis()) + "\t" + line).getBytes());
+            mPrintLock = false;
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+    }
+}
---
+++ app/src/main/java/huami/com/recognitiondemo/MainActivity.java
@@ -0,0 +1,92 @@
+package huami.com.recognitiondemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Environment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+    public static final String SENSOR_TYPE = "ACC";
+    public static long sStartTimestamp = 0;
+
+
+    public static File sSensorBaseFolder = null;
+    public static Date sStartDate = null;
+
+    boolean mIsRecording = false;
+
+    Button mStartBtn = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartBtn = (Button)findViewById(R.id.btn_start);
+
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            sSensorBaseFolder = new File(Environment.getExternalStorageDirectory(), "Recognition");
+            sSensorBaseFolder.mkdir();
+        }
+    }
+
+    private void startRecord() {
+        sStartDate = new Date();
+        sStartTimestamp = sStartDate.getTime();
+        if (!sSensorBaseFolder.exists())
+            sSensorBaseFolder.mkdir();
+
+        mStartBtn.setText("Stop");
+        mIsRecording = true;
+        startSensorService(SENSOR_TYPE);
+    }
+
+    private void stopRecord(){
+        if(mIsRecording){
+            mStartBtn.setText("Start");
+            mIsRecording = false;
+            stopSensorService(SENSOR_TYPE);
+        }
+    }
+
+    private void startSensorService(String name){
+        Intent mIntent = new Intent();
+        mIntent.setAction(AccelerometerService.ACTION_HEADER + name);
+        mIntent.setPackage(getPackageName());
+        startService(mIntent);
+    }
+
+    private void stopSensorService(String name){
+        Intent mIntent = new Intent();
+        mIntent.setAction(AccelerometerService.ACTION_HEADER + name);
+        mIntent.setPackage(getPackageName());
+        stopService(mIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopRecord();
+        super.onDestroy();
+    }
+
+    public void onStartBtnClick(View source) throws IOException{
+        if(!mIsRecording)
+            startRecord();
+        else
+            stopRecord();
+    }
+}
